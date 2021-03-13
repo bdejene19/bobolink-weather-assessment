eaa68ce0077147d9b2bbe654e35b328824153947
Bemnet Dejene <bdejene19@gmail.com>
2021-03-13 14:05:16 -0500

    refactor code

--- src/App.js
+++ src/App.js
@@ -4,35 +4,6 @@ import React, {useEffect, useState} from 'react';
 import Weather from './components/Weather'
 
 function App() {
-
-  // const createList = arr => {
-    
-  //   let output = arr.map((item) => <li>{item.room_type}, {item.vacant_rooms}, ${item.price}</li>)
-  //   return <ol>{output}</ol>
-  // }
-  // const [city, setCity] = useState('London');
-  // const fooBar = num => {
-  //   if (num % 2 === 0) {
-  //     console.log('foo');
-  //   }
-  //   if (num % 7 === 0) {
-  //     console.log('bar');
-  //   }
-
-  //   if (num % 14 === 0) {
-  //     console.log("foobar")
-  //   }
-
-  //   else {
-  //     console.log(num);
-  //   }
-  // }
-
-  // let rooms = [
-  //   { room_type: "Queen", vacant_rooms: 5, price: 100 },
-  //   { room_type: "Double", vacant_rooms: 3, price: 75 },
-  //   { room_type: "Twin", vacant_rooms: 8, price: 60 }
-  // ];
   const [city, setCity] = useState("London");
   const [data, setData] = useState();
   const getWeatherAPI = async () => {
@@ -40,20 +11,14 @@ function App() {
     let APIkey = '2d2b35d0320249fb939152037211203';
     let queryCity = `&q=${city}`;
     let response = await (await fetch(baseURL + APIkey + queryCity)).json();
-    // console.log(response);
     setData(response);
   }
 
   useEffect(() => {
     getWeatherAPI();
-    console.log('my api response data: ', data);
-
   }, [city])
-  // getWeatherAPI();
   return (
     <div className="App">
-      {/* {createList(rooms)}
-      {fooBar(28)} */}
       <br></br>
       <input type='text' value={city} onChange={e => setCity(e.target.value)} placeholder='Enter City Name'></input>
       {data.error ? <h3>Search for a city</h3> : 
